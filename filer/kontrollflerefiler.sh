48cca9b335c8638dd1041d7d4e323820109b35bd
Pål Gjerde <paalvaa@gmail.com>
2015-08-26 12:18:04 +0200

    Kommentar til kontrollflerefiler.sh.

--- filer/kontrollflerefiler.sh
+++ filer/kontrollflerefiler.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+# Når programmet starter, starter det en "filkontroll.sh"-prosess for hver fil
+# som skal overvåkes. Output fra alle disse filene omdirigeres til samme fil.
+# PIDene til disse prosessene lagres midlertidig. Etter å ha startet alle disse,
+# startes en enkel tail -f som overvåker denne filen, og outputter endringer til
+# terminal. Dette hindrer programmet i å gå videre, siden denne avsluttes med
+# Ctrl+C. For å avslutte de andre prosessene, fanges Ctrl+C opp og brukes til å
+# avslutte disse, før programmet avsluttes.
+
 pids=()
 ctrlc()
 {
