32cb049e2a3c16e6d72148bdcf3737ccc3a8142b
Pål Gjerde <paalvaa@gmail.com>
2015-08-31 14:18:08 +0200

    Hadde gjort oppg 16 feil. Fikset.

--- filer/filkontroll.sh
+++ filer/filkontroll.sh
@@ -7,22 +7,26 @@ if [ -f $filnavn ]; then
 	fil_finnes=true
 	siste_timestamp=`date -r $filnavn`
 fi
-while true; do
+sjekk=true
+while $sjekk; do
     sleep $intervall
     if ! $fil_finnes && [ -f $filnavn ]; then
         echo "Filen $filnavn har blitt opprettet."
         siste_timestamp=`date -r $filnavn`
         fil_finnes=true
+        sjekk=true
     fi
     if $fil_finnes && [ ! -f $filnavn ]; then
         echo "Filen $filnavn har blitt slettet."
         fil_finnes=false
+        sjekk=true
     fi
     if $fil_finnes; then
         nytt_timestamp=`date -r $filnavn`
         if [ "$siste_timestamp" != "$nytt_timestamp" ]; then
             echo "Filen $filnavn har blitt endret."
             siste_timestamp=$nytt_timestamp
+            sjekk=true
         fi
     fi
 done
